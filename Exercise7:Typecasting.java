f9109e91c380009d392b14908da7b6b07bbb7b54
MadhumithaRamamoorthy <csbs230430@smvec.ac.in>
2026-06-02 14:10:06 +0530

    Add TypeCasting class for demonstrating type casting

---
+++ Exercise7:Typecasting.java
@@ -0,0 +1,12 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        double d = 15.75;
+        int i = (int) d;
+
+        int num = 20;
+        double d2 = (double) num;
+
+        System.out.println("Double to Int: " + i);
+        System.out.println("Int to Double: " + d2);
+    }
+}
