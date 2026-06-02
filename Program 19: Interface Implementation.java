5da942bec62e8294208d2cb55b9223fa44e2221b
MadhumithaRamamoorthy <csbs230430@smvec.ac.in>
2026-06-02 15:16:25 +0530

    Implement Playable interface for Guitar and Piano

---
+++ Program 19: Interface Implementation.java
@@ -0,0 +1,39 @@
+interface Playable {
+
+    void play();
+}
+
+class Guitar
+implements Playable {
+
+    public void play() {
+        System.out.println(
+            "Playing Guitar"
+        );
+    }
+}
+
+class Piano
+implements Playable {
+
+    public void play() {
+        System.out.println(
+            "Playing Piano"
+        );
+    }
+}
+
+class TestInterface {
+
+    public static void main(String[] args) {
+
+        Playable g =
+            new Guitar();
+
+        Playable p =
+            new Piano();
+
+        g.play();
+        p.play();
+    }
+}
