65266181048a47153a537857626522628068255b
MadhumithaRamamoorthy <csbs230430@smvec.ac.in>
2026-06-02 15:24:43 +0530

    Create Program 23: File Reading.java

---
+++ Program 23: File Reading.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
+class FileReadDemo {
+
+    public static void main(String[] args)
+            throws Exception {
+
+        File file =
+            new File("output.txt");
+
+        Scanner sc =
+            new Scanner(file);
+
+        while(sc.hasNextLine()) {
+
+            System.out.println(
+                sc.nextLine()
+            );
+        }
+
+        sc.close();
+    }
+}
