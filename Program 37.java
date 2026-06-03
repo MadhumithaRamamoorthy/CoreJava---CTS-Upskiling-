9185143018e74e57e54fdbd6dbe90f2cd9d4b6c0
MadhumithaRamamoorthy <csbs230430@smvec.ac.in>
2026-06-03 10:18:34 +0530

    Add Sample class with show method

---
+++ Program 37.java
@@ -0,0 +1,14 @@
+Sample.java
+public class Sample {
+
+    public void show() {
+
+        System.out.println(
+            "Hello"
+        );
+    }
+}
+Commands
+javac Sample.java
+
+javap -c Sample
